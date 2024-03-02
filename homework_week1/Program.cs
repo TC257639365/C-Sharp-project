9ae3b79f13c16c31c861b08d2f7266efd44a38ed
TC257639365 <257639365@qq.com>
2024-03-03 00:48:55 +0800

    hw1

---
+++ homework_week1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace pro1
+{ 
+    public class Test
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
